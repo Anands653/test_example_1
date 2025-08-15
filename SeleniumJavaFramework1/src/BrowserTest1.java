22745579f3365a5d21a7538d1cf0a720a58dc116
Anand <anandselvaraj653@gmail.com>
2025-08-15 16:42:47 +0100

    update

--- SeleniumJavaFramework1/src/BrowserTest1.java
+++ SeleniumJavaFramework1/src/BrowserTest1.java
@@ -5,18 +5,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BrowserTest1 {
 	
-	  public static void main(String[] args) {
+	  public static void main(String[] args) throws InterruptedException {
 		
 		  
 		  WebDriver driver = new ChromeDriver();
-		  
+		  // go to flipkart.com
 		  driver.get("https://www.flipkart.com/");
 		  
-		  driver.findElement(By.name("q")).sendKeys("Mobile");
+		  // search keyword Mobile in search bar 
+		 	  driver.findElement(By.name("q")).sendKeys("Mobile");
 		  
-		  //WebElement textBox = driver.findElement(By.id("Pke_EE")).sendKeys("Mobile");
-		  
-		  
+				 	  
+		 	  // to get time to see
+		 	  
+		 	  Thread.sleep(3000);
+			  
+				 // to close the chrome browser
+				  driver.quit();	  
+		 	  
+		 	  System.out.println("Test pass");
 	}
 	  
 
